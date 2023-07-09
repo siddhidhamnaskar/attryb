90ac2d56dba311fa0c1cbbdb99feb045c379856c
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-07-09 16:42:37 +0530

    done

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -22,13 +22,13 @@ cloudinary.config({
 
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "./uploads");
-    },
-    filename: (req, file, cb) => {
-      cb(null, file.originalname);
-    },
-  });
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+})
   
   
 
