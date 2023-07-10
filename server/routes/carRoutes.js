f161034d272e7860a36ff7749eb23861b8087283
Siddhi Dhamnaskar <siddhidhamnaskar64@gmail.com>
2023-07-11 02:25:37 +0530

    done

--- server/routes/carRoutes.js
+++ server/routes/carRoutes.js
@@ -1,5 +1,5 @@
 const cars=require("../models/carModel")
-const multer=require('multer');
+
 const router=require("express");
 
 
@@ -21,30 +21,7 @@ cloudinary.config({
 
 
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads')
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-})
-  
-  
 
-
-  const fileFilter=(req,file,cb)=>{
-    if(file.mimetype==='image/jpeg' || file.mimetype==='image/png')
-    {
-        cb(null,true)
-    }
-    else
-    {
-        cb({message:"Unsuported File Format"},false)
-    }
-  }
-
-  const upload = multer({ storage: storage});
 
 
 
